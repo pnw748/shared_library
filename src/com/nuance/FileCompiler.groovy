8e6931fff4444b62ea71c9208db14eb9fdbf0289
Shanghai Fu <shanghai.fu@nuance.com>
2017-09-28 17:57:19 +0800

    test

--- src/com/nuance/FileCompiler.groovy
+++ src/com/nuance/FileCompiler.groovy
@@ -16,15 +16,17 @@ class FileCompiler implements Serializable {
 
     def verifpara(String s2version) {
         int buildNum = s2version.toInteger()
-        if (buildNum > 3880)
-        {
-            echo "Build ${buildNum} is new enough to build directly on the grid."
-            return true
-        }
-        else
-        {
-            echo "Build ${buildNum} is old enough that we will build on our blade."
-            return false
-        }
+        // if (buildNum > 3880)
+        // {
+        //     echo "Build ${buildNum} is new enough to build directly on the grid."
+        //     return true
+        // }
+        // else
+        // {
+        //     echo "Build ${buildNum} is old enough that we will build on our blade."
+        //     return false
+        // }
+        return buildNum
+    
     }
 }
