d60bb36ac260df4c9b49514504d850b7b847eb4f
Shanghai Fu <shanghai.fu@nuance.com>
2017-09-28 18:15:18 +0800

    8888

--- src/com/nuance/FileCompiler.groovy
+++ src/com/nuance/FileCompiler.groovy
@@ -27,6 +27,20 @@ class FileCompiler implements Serializable {
         //     return false
         // }
         return buildNum
+    }
     
+    def static verifpara_sta(String s2version) {
+        int buildNum = s2version.toInteger()
+        // if (buildNum > 3880)
+        // {
+        //     echo "Build ${buildNum} is new enough to build directly on the grid."
+        //     return true
+        // }
+        // else
+        // {
+        //     echo "Build ${buildNum} is old enough that we will build on our blade."
+        //     return false
+        // }
+        return buildNum
     }
 }
