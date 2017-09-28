540a5d9716f4706b97ec0d63742436a0061d11cf
Shanghai Fu <13882261570@163.com>
2017-09-29 00:49:45 +0800

    update for demo_function

--- src/com/nuance/Utility.groovy
+++ src/com/nuance/Utility.groovy
@@ -2,21 +2,7 @@ package com.nuance
 
 public class Utility {
     
-    def static verifpara(String s2version) {
-        //int buildNum = s2version.substring(10) as Integer
-        int buildNum = s2version.toInteger()
-        println "abcaaaaaa"
-        //echo ${buildNum}
-        // if (buildNum > 3880)
-        // {
-        //     echo "Build ${buildNum} is new enough to build directly on the grid."
-        //     return true
-        // }
-        // else
-        // {
-        //     echo "Build ${buildNum} is old enough that we will build on our blade."
-        //     return false
-        // }
-        return buildNum
+    def static demo_function(String input) {
+        return "hello ${input}"
     }
 }
