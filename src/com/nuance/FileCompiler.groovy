f47c09fc3ed967a945fca6832b01207e5b5be4e2
Shanghai Fu <shanghai.fu@nuance.com>
2017-09-28 17:19:05 +0800

    test

--- src/com/nuance/FileCompiler.groovy
+++ src/com/nuance/FileCompiler.groovy
@@ -3,7 +3,7 @@ package com.nuance
 class FileCompiler implements Serializable {
     def script  
     def project
-    FileCompiler(script, String project){     
+    FileCompiler(script, String project) {     
         this.script = script   
         this.project = project
         this.script.echo("Compiling ${project}")
@@ -12,5 +12,19 @@ class FileCompiler implements Serializable {
     def analyze(String file) {
         this.script.echo("${project}/${file}")    
         this.script.sh("pwd")
+    }
+
+    def ModuleNewS2(String s2version) {
+        int buildNum = s2version.substring(10) as Integer
+        if (buildNum > 3880)
+        {
+            echo "Build ${buildNum} is new enough to build directly on the grid."
+            return true
+        }
+        else
+        {
+            echo "Build ${buildNum} is old enough that we will build on our blade."
+            return false
+        }
     }
 }
--- vars/compileFile.groovy
+++ vars/compileFile.groovy
@@ -4,4 +4,7 @@ def call(String project) {
     fc = new FileCompiler(this, project)
     fc.analyze('requirements.txt')
     fc.analyze('setup.py')
+
+    //int dblInt = fc.ModuleNewS2('4000')
+    //println dblInt
 }
