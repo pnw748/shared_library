96f9ca2347ea306dc7dfcef907b0189c91914a3d
Shanghai Fu <shanghai.fu@nuance.com>
2017-09-28 18:35:14 +0800

    add groovy

---
+++ src/com/nuance/Utility.groovy
@@ -0,0 +1,19 @@
+package com.nuance
+
+class Utility {
+    
+    def verifpara(String s2version) {
+        int buildNum = s2version.toInteger()
+        // if (buildNum > 3880)
+        // {
+        //     echo "Build ${buildNum} is new enough to build directly on the grid."
+        //     return true
+        // }
+        // else
+        // {
+        //     echo "Build ${buildNum} is old enough that we will build on our blade."
+        //     return false
+        // }
+        return buildNum
+    }
+}
